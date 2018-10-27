e08329fc7e4e621f30d58a36b383a0eff09ffc43
DarkSlow <44510159+DarkSlow@users.noreply.github.com>
2018-10-27 12:06:46 -0500

    Primer Commit

---
+++ src/Clases/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Simon_PC
+ */
+public class Principal 
+{
+    public static void main(String[] args) {
+        Principal prin = new Principal();
+        prin.imprime();
+    }
+    
+    void imprime(){
+        System.out.println("Hola Mundo");
+    }
+    
+}
